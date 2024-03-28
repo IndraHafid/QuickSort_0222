ef968687f4467b6fbf808ea490a6501c5d9fc478
IndraHfid <indrahafidsaputra@gmail.com>
2024-03-28 15:09:00 +0700

    //Step 13

--- Quick Sort/Quick Sort.cpp
+++ Quick Sort/Quick Sort.cpp
@@ -83,6 +83,10 @@ void q_short(int low, int high)
 
     //recursive call to sort the leftnsub array
     q_short(low, j - 1); //step 12
+
+    //recursuve call to sort the right sub array
+
+    q_short(j + 1, high); //step 13
 }
 
 int main()
