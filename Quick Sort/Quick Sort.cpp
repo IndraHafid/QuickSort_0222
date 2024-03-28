909c8ef76580167d33ec91702fb8e2a88453ca80
IndraHfid <indrahafidsaputra@gmail.com>
2024-03-28 14:19:37 +0700

    Add project files.

---
+++ Quick Sort/Quick Sort.cpp
@@ -0,0 +1,18 @@
+// Quick Sort.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int arr[20];
+int cmp_count = 0;
+int mov_count = 0;
+int n;
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
