f0e994ab614a876142f1586f5fe7dd7a7c51f099
IndraHfid <indrahafidsaputra@gmail.com>
2024-03-28 15:04:22 +0700

    //Step 10

--- Quick Sort/Quick Sort.cpp
+++ Quick Sort/Quick Sort.cpp
@@ -54,25 +54,31 @@ void q_short(int low, int high)
     i = low + 1; //step 3
     j = high; //step 4
 
- {
-//search for an element greater than pivot
-    while ((arr[i] <= pivot) && (i <= high)) //step 5
+    while (i <= j) //step 10
     {
-        i++; //step 6
+       
+        //search for an element greater than pivot
+        while ((arr[i] <= pivot) && (i <= high)) //step 5
+        {
+           i++; //step 6
+           cmp_count++;
+        }
         cmp_count++;
-    }
-    cmp_count++;
-    //search for an element less than or equal to pivot
-    while ((arr[j] > pivot) && (j >= low)) //step 7
-    {
-        j--; //step 8
-        cmp_count++;
+        //search for an element less than or equal to pivot
+        while ((arr[j] > pivot) && (j >= low)) //step 7
+        {
+            j--; //step 8
+            cmp_count++;
     }
     cmp_count++;
     if (i < j) //step 9
+       {
+           swap(i, j);
 
+       }
     }
 }
+
 int main()
 {
     std::cout << "Hello World!\n";
