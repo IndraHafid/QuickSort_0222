b73375d7f03726fa0c4cc1cd600cf9162477cfd3
IndraHfid <indrahafidsaputra@gmail.com>
2024-03-28 14:26:07 +0700

    Menginput panjang element array

--- Quick Sort/Quick Sort.cpp
+++ Quick Sort/Quick Sort.cpp
@@ -9,6 +9,30 @@ int cmp_count = 0;
 int mov_count = 0;
 int n;
 
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan panjang element array:";
+        cin >> n;
+
+        if (n <= 20)
+            break;
+        else
+            cout << "\nMaksimum panjang array adalah 20" << endl;
+    }
+    cout << "\n_______" << endl;
+    cout << "\nEnter Array Element" << endl;
+    cout << "\n_______" << endl;
+
+    for (int i = 0; i < n; i++)
+    {
+        cout << "<" << (i + 1) << ">";
+        cin >> arr[i];
+    }
+}
+
+
 int main()
 {
     std::cout << "Hello World!\n";
